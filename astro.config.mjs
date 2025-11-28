a3756c60b894483a6f0cf2b6609f09b9ca295a9d
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-11-28 23:19:44 +0000

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,21 +6,13 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+			title: 'OmgRod AltStore Source',
+			tableOfContents: false,
+			sidebar: [],
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/OmgRod/altstore' }
 			],
+			pagination: false
 		}),
 	],
 });
